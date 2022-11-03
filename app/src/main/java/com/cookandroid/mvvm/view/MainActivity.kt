5012e58e38162ddca3da433746c48a9c0c91f4df
915dbfl <dbfl00915@naver.com>
2022-11-03 20:59:19 +0900

    implement mvvm with databinding(layout)

--- app/src/main/java/com/cookandroid/mvvm/view/MainActivity.kt
+++ app/src/main/java/com/cookandroid/mvvm/view/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.cookandroid.mvvm.view
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.cookandroid.mvvm.R
 import com.cookandroid.mvvm.databinding.ActivityMainBinding
 import com.cookandroid.mvvm.viewmodel.OrderViewModel
 
@@ -19,38 +14,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         viewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
-        setAppleQtyObserver()
-        setTotalPriceObserver()
-        setDecreaseButtonClickListener()
-        setIncreaseButtonClickListener()
-    }
-
-    private fun setAppleQtyObserver(){
-        viewModel.appleQuantity.observe(this){
-            binding.appleCountText.text = it
-        }
-    }
-
-    private fun setTotalPriceObserver(){
-        viewModel.totalPrice.observe(this){
-            binding.totalCountText.text = it
-        }
-    }
-
-    private fun setIncreaseButtonClickListener(){
-        binding.increaseAppleButton.setOnClickListener {
-            viewModel.addApple()
-        }
-    }
-
-    private fun setDecreaseButtonClickListener(){
-        binding.decreaseAppleButton.setOnClickListener {
-            viewModel.decreaseApple()
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
     }
 
 }
